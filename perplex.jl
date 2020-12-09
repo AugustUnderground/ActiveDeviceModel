bd080c477a785a11ed59112690477161d2f08194
Yannick Uhlmann <augustunderground@protonmail.com>
2020-12-09 10:44:24 +0100

    more plots

--- perplex.jl
+++ perplex.jl
@@ -14,7 +14,7 @@ macro bind(def, element)
 end
 
 # ╔═╡ bf21b8ec-357f-11eb-023f-6b64f6e0da73
-using DataFrames, JLD2, Plots, PlutoUI
+using DataFrames, StatsBase, JLD2, Plots, PlutoUI
 
 # ╔═╡ 9f08514e-357f-11eb-2d48-a5d0177bcc4f
 begin
@@ -23,7 +23,7 @@ begin
 end
 
 # ╔═╡ d091d5e2-357f-11eb-385b-252f9ee49070
-simData = jldopen("../data/ptmn.jld") do file
+simData = jldopen("../data/ptmn45.jld") do file
 	file["database"];
 end;
 
@@ -34,7 +34,7 @@ simData.Vgs = round.(simData.Vgs, digits = 2);
 begin
 	slVds = @bind vds Slider( 0.01 : 0.01 : 1.20
 							, default = 0.6, show_value = true );
-	slW = @bind w Slider( 5.0e-7 : 1.0e-7 : 5.0e-6
+	slW = @bind w Slider( 7.5e-7 : 2.5e-7 : 5.0e-6
 						, default = 5.0e-7, show_value = true );
 	slL = @bind l Slider( 1.5e-7 : 1.0e-7 : 1.5e-6
 						, default = 1.5e-7, show_value = true );
@@ -52,70 +52,98 @@ end
 begin
 	dd = simData[ ( (simData.Vds .== vds)
 			 	 .& (simData.W .== w) )
-				, ["W", "L", "gm", "gds", "id", "vdsat"] ];
+				, ["W", "L", "gm", "gds", "id", "vdsat", "fug"] ];
 	dd.idw = dd.id ./ dd.W;
 	dd.gmid = dd.gm ./ dd.id;
 	dd.a0 = dd.gm ./ dd.gds;
 end;
 
-# ╔═╡ 42777980-3584-11eb-2f89-91d4fa8683d3
+# ╔═╡ 24a21870-360b-11eb-1269-db94fecdb0a6
 begin
 	idwgmid = plot();
 	for len in 1.5e-7 : 1.0e-7 : 1.5e-6
 		idwgmid = plot!( dd[dd.L .== len, "gmid"]
-			 	   , dd[dd.L .== len, "idw"]
-			 	   , yscale = :log10
-				   , lab = "L = " *string(len)
-				   , legend = false
-			 	   , yaxis = "id/W", xaxis = "gm/id" );
+			 	   	   , dd[dd.L .== len, "idw"]
+			 	   	   , yscale = :log10
+				   	   , lab = "L = " *string(len)
+				       , legend = false
+			 	       , yaxis = "id/W", xaxis = "gm/id" );
 	end;
 	idwgmid
 end;
 
-# ╔═╡ fcb28aca-3586-11eb-0c54-bfda0035451d
-begin
-	a0gmid = plot();
-	for len in 1.5e-7 : 1.0e-7 : 1.5e-6
-		a0gmid = plot!( dd[dd.L .== len, "gmid"]
-			 	   , dd[dd.L .== len, "a0"]
-			 	   , yscale = :log10
-				   , lab = "L = " *string(len)
-				   , legend = false
-			 	   , yaxis = "A0", xaxis = "gm/id" );
-	end;
-	a0gmid
-end;
-
-# ╔═╡ 439f0ba4-3587-11eb-3861-bd9ab7650012
+# ╔═╡ c6232b50-360b-11eb-18a2-39bdc25fb03b
 begin
 	idwvdsat = plot();
 	for len in 1.5e-7 : 1.0e-7 : 1.5e-6
 		idwvdsat = plot!( dd[dd.L .== len, "vdsat"]
-			 	   , dd[dd.L .== len, "idw"]
-			 	   , yscale = :log10
-				   , lab = "L = " *string(len)
-				   , legend = false
-			 	   , yaxis = "id/W", xaxis = "vdsat" );
+			 	       	, dd[dd.L .== len, "idw"]
+			 	   		, yscale = :log10
+				   		, lab = "L = " *string(len)
+				   		, legend = false
+			 	   		, yaxis = "id/W", xaxis = "vdsat" );
 	end;
 	idwvdsat
 end;
 
-# ╔═╡ 587ae052-3587-11eb-1621-2b51c52e17e8
+# ╔═╡ cff6fad6-360b-11eb-3e9b-a7cf6a270f8f
+begin
+	a0gmid = plot();
+	for len in 1.5e-7 : 1.0e-7 : 1.5e-6
+		a0gmid = plot!( dd[dd.L .== len, "gmid"]
+			 	   	  , dd[dd.L .== len, "a0"]
+			 	      , yscale = :log10
+				      , lab = "L = " *string(len)
+				      , legend = false
+			 	      , yaxis = "A0", xaxis = "gm/id" );
+	end;
+	a0gmid
+end;
+
+# ╔═╡ d1b49f7e-360b-11eb-2b4d-b5a6ab46505e
 begin
 	a0vdsat = plot();
 	for len in 1.5e-7 : 1.0e-7 : 1.5e-6
 		a0vdsat = plot!( dd[dd.L .== len, "vdsat"]
-			 	   , dd[dd.L .== len, "a0"]
-			 	   , yscale = :log10
-				   , lab = "L = " *string(len)
-				   , legend = false
-			 	   , yaxis = "A0", xaxis = "vdsat" );
+			 	   	   , dd[dd.L .== len, "a0"]
+			 	   	   , yscale = :log10
+				   	   , lab = "L = " *string(len)
+				   	   , legend = false
+			 	   	   , yaxis = "A0", xaxis = "vdsat" );
 	end;
 	a0vdsat
 end;
 
+# ╔═╡ d34046d6-360b-11eb-31cd-6378f8c1729c
+begin
+	ftgmid = plot();
+	for len in 1.5e-7 : 1.0e-7 : 1.5e-6
+		ftgmid = plot!( dd[dd.L .== len, "gmid"]
+			 	   	  , dd[dd.L .== len, "fug"]
+			 	   	  , yscale = :log10
+				   	  , lab = "L = " *string(len)
+				   	  , legend = false
+			 	   	  , yaxis = "fug", xaxis = "gmid" );
+	end;
+	ftgmid
+end;
+
+# ╔═╡ d46c5e3c-360b-11eb-3ab7-9dc5eeb107d6
+begin
+	ftvdsat = plot();
+	for len in 1.5e-7 : 1.0e-7 : 1.5e-6
+		ftvdsat = plot!( dd[dd.L .== len, "vdsat"]
+			 	   	   , dd[dd.L .== len, "fug"]
+			 	   	   , yscale = :log10
+				   	   , lab = "L = " *string(len)
+				   	   , legend = false
+			 	   	   , yaxis = "fug", xaxis = "vdsat" );
+	end;
+	ftvdsat
+end;
+
 # ╔═╡ 293aad98-3587-11eb-0f56-1d8144ad7e84
-plot(idwgmid, idwvdsat, a0gmid, a0vdsat, layout = (2,2))
+plot(idwgmid, idwvdsat, a0gmid, a0vdsat, ftgmid, ftvdsat, layout = (3,2))
 
 # ╔═╡ 0282c34c-3580-11eb-28c5-e5badd2c345f
 df = simData[ ( (simData.Vds .== vds)
@@ -130,10 +158,33 @@ begin
 	df.a0 = df.gm ./ df.gds;
 end;
 
-# ╔═╡ 88f76a24-3580-11eb-297a-2fe3c8a9b083
-plot( df.gmid, df.idw
-	, yscale = :log10
-	, yaxis = "id/W", xaxis = "gm/id")
+# ╔═╡ 58557ff8-39e8-11eb-2c11-bd4826addc39
+begin
+	dfW = simData[ (simData.W .== 2e-6)
+				 , ["W", "L", "Vds", "id", "vdsat", "gm", "gds", "fug"] ];
+	dfW.idW = dfW.id ./ dfW.W;
+	dfW.A0 = dfW.gm ./ dfW.gds;
+	dfW.gmid = dfW.gm ./ dfW.id;
+end;
+
+# ╔═╡ c33db09e-39e8-11eb-360e-e7ffc4b16d6e
+begin
+	op = dfW[dfW.Vds .== 0.6,:];
+	op.vdsat = round.(op.vdsat, digits = 4);
+end
+
+# ╔═╡ 913c5094-39f2-11eb-0e7e-f9c44892fb1c
+round.(op.vdsat, digits = 4)
+
+# ╔═╡ 20ff39aa-39e9-11eb-3fdc-a7412c6fe023
+begin
+	plt = plot();
+	[ ( opl = op[op.L .== l, :];
+	    plt = plot!( opl.vdsat, (opl.id ./ opl.W)
+				   , yscale = :log10))
+	  for l in unique(op.L) ];
+	plt
+end
 
 # ╔═╡ Cell order:
 # ╠═9f08514e-357f-11eb-2d48-a5d0177bcc4f
@@ -143,10 +194,15 @@ plot( df.gmid, df.idw
 # ╠═293aad98-3587-11eb-0f56-1d8144ad7e84
 # ╠═a002f77c-3580-11eb-0ad8-e946d85c84c7
 # ╠═092d49d4-3584-11eb-226b-bde1f2e49a22
-# ╠═42777980-3584-11eb-2f89-91d4fa8683d3
-# ╠═fcb28aca-3586-11eb-0c54-bfda0035451d
-# ╠═439f0ba4-3587-11eb-3861-bd9ab7650012
-# ╠═587ae052-3587-11eb-1621-2b51c52e17e8
+# ╠═24a21870-360b-11eb-1269-db94fecdb0a6
+# ╠═c6232b50-360b-11eb-18a2-39bdc25fb03b
+# ╠═cff6fad6-360b-11eb-3e9b-a7cf6a270f8f
+# ╠═d1b49f7e-360b-11eb-2b4d-b5a6ab46505e
+# ╠═d34046d6-360b-11eb-31cd-6378f8c1729c
+# ╠═d46c5e3c-360b-11eb-3ab7-9dc5eeb107d6
 # ╠═0282c34c-3580-11eb-28c5-e5badd2c345f
 # ╠═6b97b4f0-3580-11eb-28e5-b356737b0905
-# ╠═88f76a24-3580-11eb-297a-2fe3c8a9b083
+# ╠═58557ff8-39e8-11eb-2c11-bd4826addc39
+# ╠═c33db09e-39e8-11eb-360e-e7ffc4b16d6e
+# ╠═913c5094-39f2-11eb-0e7e-f9c44892fb1c
+# ╠═20ff39aa-39e9-11eb-3fdc-a7412c6fe023
